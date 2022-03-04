da661ea15d108b52107cc38762bb0a02b901113e
Rushikesh Jalvi <rushikeshjalvi23@gmail.com>
2022-03-04 14:22:01 +0000

    dropdown binding

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -9,38 +9,47 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("sap.ui.inventory.controller.app", {
+
             onInit: function () {
+                console.log(this.getOwnerComponent().getModel())
                 var oData = {
                     "available": [
                         {
+                            "ID": 1,
                             "key": "yes",
                             "value": "Yes"
                         }, 
                         {
+                            "ID": 2,
                             "key": "no",
                             "value": "No"
                         }
                     ],
                     "inoperation": [
                         {
+                            "ID": 1,
                             "key": "yes",
                             "value": "Yes"
                         },
                         {
+                            "ID": 2,
                             "key": "less",
                             "value": "No (<1 year)"
                         },
                         {
+                            "ID": 3,
                             "key": "more",
                             "value": "No (>1 year)"
                         }
                     ],
                     "other": [
                         {
+                            "ID": 1,
                             "key": "updated",
                             "value": "Asset data to be updated"
                         },
                         {
+                            "ID": 2,
                             "key": "inventoried",
                             "value": "Asset cannot be Inventoried"
                         },
@@ -53,12 +62,23 @@ sap.ui.define([
                 var colCount = 7;
                 oTable.setFixedColumnCount(colCount);
             },
-            // onSelection: function(event){
-            //     var oValue = event.getParameter("selectedItem").getParent().getValue()
-            //     var path =event.getSource().getBindingContext().getPath();
-            //     this.getView().byId("_IDGenObjectStatus2").setEnabled(false)
-            //     console.log(path)
-            // },
+            onSelection: function(event){
+                debugger
+                var oDataModel = this.getOwnerComponent().getModel()
+                //var oDataModel = event.getSource();                
+                var oValue = event.getParameter("selectedItem").getParent().getValue()
+                var path = event.getSource().getBindingContext().getPath();
+                //var value = oDataModel.getProperty(path)
+                //path = path.slice(1,path.length)
+                console.log(path)
+                
+                console.log(oDataModel.getProperty(path))
+                // if (oValue === "No") {
+                //     oDataModel.getObject(path).showAssetInOpertnDpDn = false;
+                // } else {
+                //     oDataModel.getObject(path).showAssetInOpertnDpDn = true;
+                // }
+            },
             // onSelection1: function(event){
             //     var oValue = event.getParameter("selectedItem").getParent().getValue()
             //     var path =event.getSource().getBindingContext().getPath();
