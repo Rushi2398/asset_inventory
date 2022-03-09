c33bccb13ae821f2e436e026d9d544ba55f90d6b
Rushikesh Jalvi <rushikeshjalvi23@gmail.com>
2022-03-09 11:46:58 +0000

    Mass Update

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,55 +1,50 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, MessageBox, Fragment) {
         "use strict";
 
         return Controller.extend("sap.ui.inventory.controller.app", {
 
             onInit: function () {
-                console.log(this.getOwnerComponent().getModel())
+                
                 var oData = {
                     "available": [
                         {
-                            "ID": 1,
                             "key": "yes",
                             "value": "Yes"
                         }, 
                         {
-                            "ID": 2,
                             "key": "no",
                             "value": "No"
                         }
                     ],
                     "inoperation": [
                         {
-                            "ID": 1,
                             "key": "yes",
                             "value": "Yes"
                         },
                         {
-                            "ID": 2,
                             "key": "less",
                             "value": "No (<1 year)"
                         },
                         {
-                            "ID": 3,
                             "key": "more",
                             "value": "No (>1 year)"
                         }
                     ],
                     "other": [
                         {
-                            "ID": 1,
                             "key": "updated",
                             "value": "Asset data to be updated"
                         },
                         {
-                            "ID": 2,
                             "key": "inventoried",
                             "value": "Asset cannot be Inventoried"
                         },
@@ -62,27 +57,65 @@ sap.ui.define([
                 var colCount = 7;
                 oTable.setFixedColumnCount(colCount);
             },
-            onSelection: function(event){
-                debugger
-                var oDataModel = this.getOwnerComponent().getModel()
-                //var oDataModel = event.getSource();                
-                var oValue = event.getParameter("selectedItem").getParent().getValue()
+            onAvailabilitySelection: function(event){
+                var oDataModel = this.getOwnerComponent().getModel();               
+                var oValue = event.getParameter("selectedItem").getParent().getValue();
                 var path = event.getSource().getBindingContext().getPath();
-                //var value = oDataModel.getProperty(path)
-                //path = path.slice(1,path.length)
-                console.log(path)
-                
-                console.log(oDataModel.getProperty(path))
-                // if (oValue === "No") {
-                //     oDataModel.getObject(path).showAssetInOpertnDpDn = false;
-                // } else {
-                //     oDataModel.getObject(path).showAssetInOpertnDpDn = true;
-                // }
+                if (oValue === "No") {
+                    oDataModel.setProperty(path+"/showAssetInOpertnDpDn",false)
+                } else {
+                    oDataModel.setProperty(path+"/showAssetInOpertnDpDn",true)
+                }
             },
-            // onSelection1: function(event){
-            //     var oValue = event.getParameter("selectedItem").getParent().getValue()
-            //     var path =event.getSource().getBindingContext().getPath();
-            //     console.log(path)
-            // }
+            onMassUpdatePress : function () {
+                var oTable = this.getView().byId("_IDGenTable1");
+                var selectIndices = oTable.getSelectedIndices();
+                if(selectIndices.length != 0){
+                    this._getDialog().open();
+                } else {
+                    MessageBox.alert("Select atleast one row for mass update!");
+                }
+            },
+            _getDialog : function () {
+                if (!this._oDialog) {
+                    this._oDialog = sap.ui.xmlfragment("sap.ui.inventory.fragment.massUpdate", this);
+                    this.getView().addDependent(this._oDialog);
+                }
+                return this._oDialog;
+            },
+            saveDialog: function(oEvent) {
+                var available = sap.ui.getCore().byId("_IDGenComboStatus1").getValue();
+                var inOperation = sap.ui.getCore().byId("_IDGenComboStatus2").getValue();
+                var other = sap.ui.getCore().byId("_IDGenComboStatus3").getValue();
+                var oTable = this.getView().byId("_IDGenTable1");
+                var rows = oTable.getRows();
+                var selectIndices = oTable.getSelectedIndices();
+                for( var i of selectIndices){
+                    var  path = rows[i].getBindingContext().getPath();
+                    var oDataModel = this.getOwnerComponent().getModel();
+                    if(available === "No"){
+                        oDataModel.setProperty(path+"/Availability",available);
+                        oDataModel.setProperty(path+"/showAssetInOpertnDpDn",false);
+                        oDataModel.setProperty(path+"/Other",other);
+                    } else {
+                        oDataModel.setProperty(path+"/Availability",available);
+                        oDataModel.setProperty(path+"/showAssetInOpertnDpDn",true);
+                        oDataModel.setProperty(path+"/InOperation",inOperation);
+                        oDataModel.setProperty(path+"/Other",other);
+                    }
+                }
+                this._getDialog().close();
+            },
+            validateData : function(){
+                var available = sap.ui.getCore().byId("_IDGenComboStatus1").getValue();
+                if (available === "No"){
+                    sap.ui.getCore().getControl("_IDGenComboStatus2").setEnabled(false);
+                } else {
+                    sap.ui.getCore().getControl("_IDGenComboStatus2").setEnabled(true);
+                }
+            },
+            closeDialog: function () {
+                this._getDialog().close();
+            }
         });
     });
